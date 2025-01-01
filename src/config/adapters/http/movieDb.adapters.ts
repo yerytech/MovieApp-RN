2f9c2fa8aede52cc7812544f212e3fb05100dc95
yerytech <202000854@p.uapa.edu.do>
2025-01-01 12:40:56 -0400

    Update .env.template and movieDb.adapters.ts to use EXPO_PUBLIC_MOVIEDB_API_KEY

--- src/config/adapters/http/movieDb.adapters.ts
+++ src/config/adapters/http/movieDb.adapters.ts
@@ -1,9 +1,11 @@
 import { AxiosAdapter } from "./axios.adapter";
-// process.env.MOVIEDB_API_KEY,
+
 export const movieDBFecher = new AxiosAdapter({
   baseUrl: "https://api.themoviedb.org/3/movie",
   params: {
-    api_key: "b14ccfd4ff10749633807ea61465b00e",
+    api_key: process.env.EXPO_PUBLIC_MOVIEDB_API_KEY,
     language: "es",
   },
 });
+
+// "b14ccfd4ff10749633807ea61465b00e",
